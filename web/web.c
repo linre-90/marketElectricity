4264d4660c538022ea5d570e92bacb63623c1368
linre-90 <juho.lindemark@gmail.com>
2024-01-30 11:58:40 +0200

    web cleanup

--- web/web.c
+++ web/web.c
@@ -8,7 +8,7 @@
 #include <curl/curl.h>
 #include "web.h"
 
-/* Memory for json response. */
+/* Memory for holding curl response. */
 struct MemoryStruct {
 	char* memory;
 	size_t size;
@@ -22,31 +22,40 @@ float parseJsonResponse(const char* res, struct Price* priceArr);
 
 
 void fetchData(struct Price* priceArr) {
+	// Init curl
 	curl_global_init(CURL_GLOBAL_ALL);
 	CURL* curl = curl_easy_init();
 	CURLcode res;
+	// If curl pointer is not null
 	if (curl) {
+		// reserve memory chunk
 		struct MemoryStruct chunk;
-		chunk.memory = malloc(1);
-		chunk.size = 0;
+		chunk.memory = malloc(1); // Resized in WriteMemoryCallback
+		chunk.size = 0; // nothing is stored
+
 		if (chunk.memory == NULL) {
 			printf("Cannot allocate memory for chunk.memory.\n");
 			curl_global_cleanup();
+			return;
 		}
 
-		/* Prepare and perform request */
+		// Prepare request url
 		curl_easy_setopt(curl, CURLOPT_URL, "https://api.porssisahko.net/v1/latest-prices.json");
+		// response data is sent to WriteMemoryCallback function
 		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
+		// chunk struct is passed to WriteMemoryCallback
 		curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void*)&chunk);
+		// Perform request
 		res = curl_easy_perform(curl);
-
+		// Result code is not 200
 		if (res != CURLE_OK) {
 			fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
 		}
 		else {
+			// Succesfully retrieved data
 			parseJsonResponse(chunk.memory, priceArr);
 		}
-
+		// free mem and clean up curl.
 		free(chunk.memory);
 		curl_easy_cleanup(curl);
 		curl_global_cleanup();
@@ -58,72 +67,65 @@ float parseJsonResponse(const char* res, struct Price* priceArr) {
 	// Skip the opening { and jump right in [ character
 	int iterator = 10;
 	int priceObjIterator = 0;
-	bool parsingObject = false;
 	char searchPropertyPrice[] = "price";
 
+	// Loop over the whole res after skipping
 	while (res[iterator] != 0) {
-		// start parsing object
-		if (res[iterator] == '{' || parsingObject) {
-			parsingObject = true;
-			char priceStr[10] = { 0 };
-			char utcYYYYStr[5] = { 0 };
-			char utcDDStr[3] = { 0 };
-			char utcMMStr[3] = { 0 };
-			char utcHH[3] = { 0 };
+		// start parsing object when ever '{' character is found.
+		if (res[iterator] == '{') {
+			char priceStr[10] = { 0 };	// string buff to store price
+			char utcYYYYStr[5] = { 0 }; // string buff to store year
+			char utcDDStr[3] = { 0 };	// string buff to store day
+			char utcMMStr[3] = { 0 };	// string buff to store month
+			char utcHH[3] = { 0 };		// string buff to store hours
+			int priceStrIterator = 0;	// Prices change in length that is why this is needed
 
-			int priceStrIterator = 0;
-
-			// Fast forward to 'p' character and parse price
+			// Move forward to 'p' ([p]rice) character
 			while (res[iterator] != 'p' && res[iterator] != 0){ iterator++; }
 			iterator++;
-			// Fast forward to first number digit or -
+
+			// Move forward to first number digit or '-' (prices can be negative)
 			while (res[iterator] != ':' && res[iterator] != 0) { iterator++; }
 			iterator++;
-			// Start extracting price
+
+			// Start extracting price until ',' is found, prices do not contain quotes.
 			while (res[iterator] != ',' && res[iterator] != 0)
 			{
 				priceStr[priceStrIterator] = res[iterator];
 				iterator++;
 				priceStrIterator++;
 			}
+
 			// Set price to struct convert cents to eur
 			priceArr[priceObjIterator].price = atof(priceStr);
 
-			// Fast forward to 's' character and parse date
+			// Move forward to 's' ([s]tartDate) character and parse date
 			while (res[iterator] != 's' && res[iterator] != 0) { iterator++; }
 			iterator++;
 
 			// Fast forward to first year digit
 			while (res[iterator] != ':' && res[iterator] != 0) { iterator++; }
-			iterator+=2;
+			iterator += 2;
 			utcYYYYStr[0] = res[iterator];
 			utcYYYYStr[1] = res[iterator + 1];
 			utcYYYYStr[2] = res[iterator + 2];
 			utcYYYYStr[3] = res[iterator + 3];
-			// set year
-			//priceArr[priceObjIterator].YYYY = atoi(utcYYYYStr);
 			iterator += 3;
 
 			// Skip - and jump to first month digit
 			iterator += 2;
 			utcMMStr[0] = res[iterator];
 			utcMMStr[1] = res[iterator + 1];
-			// set month
-			//priceArr[priceObjIterator].MM = atoi(utcMMStr);
 
 			// Skip - and jump to first day digit
 			iterator += 3;
 			utcDDStr[0] = res[iterator];
 			utcDDStr[1] = res[iterator + 1];
-			// set year
-			//priceArr[priceObjIterator].DD = atoi(utcDDStr);
 			
 			// Skip t and jump to first hour digit
 			iterator += 3;
 			utcHH[0] = res[iterator];
 			utcHH[1] = res[iterator + 1];
-			// set hour
-			//priceArr[priceObjIterator].utcHour = atoi(utcHH);
 
 			// Build up unix time stamp from parsed strings.
 			struct tm m_time = {
@@ -136,14 +138,15 @@ float parseJsonResponse(const char* res, struct Price* priceArr) {
 				.tm_isdst = -1,
 			};
 
-
+			// Make time and add it to price
 			time_t utc = mktime(&m_time);
 			priceArr[priceObjIterator].utcTime = utc;
 
-			// Object parsing is finished
-			parsingObject = false;
+			// Object parsing is finished not interested in the rest of the object properties
 			priceObjIterator++;
 		}
+
+		// Just move along
 		iterator++;
 	}
 }
@@ -152,18 +155,23 @@ float parseJsonResponse(const char* res, struct Price* priceArr) {
 static size_t WriteMemoryCallback(void* contents, size_t size, size_t nmemb, void* userp)
 {
 	size_t realsize = size * nmemb;
+	// Cast userp to correct type
 	struct MemoryStruct* mem = (struct MemoryStruct*)userp;
 
+	// Realloc data + realsize + 1
 	char* ptr = realloc(mem->memory, mem->size + realsize + 1);
 	if (!ptr) {
 		/* out of memory! */
 		printf("Not enough memory (realloc returned NULL)\n");
 		return 0;
 	}
-
+	// Update ptr to memory to reallocated address
 	mem->memory = ptr;
+	// Copy contents after existing data up to realsize
 	memcpy(&(mem->memory[mem->size]), contents, realsize);
+	// Update MemoryStruct size property
 	mem->size += realsize;
+	// Set last allocated mem location to 0
 	mem->memory[mem->size] = 0;
 
 	return realsize;
--- web/web.h
+++ web/web.h
@@ -6,18 +6,13 @@
 /* How many results api returs on fetchData() */
 static const int NUM_OF_API_RESULTS = 48;
 
-/* Presents data parsed by web module json converter */
+/* Presents data parsed by web module json converter. Consists of [price] in cents and [utcTime] in unix time stamp. */
 struct Price {
 	float price;
 	time_t utcTime;
-
-	int utcHour;
-	int MM;
-	int YYYY;
-	int DD;
 };
 
-/* Fetch 48 hour price listing from API.*/
+/* Fetch latest data blop that contains 48 hours of data.*/
 void fetchData(struct Price* priceArr);
 
 #endif // !WEB_H
